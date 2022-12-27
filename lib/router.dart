9205c3d8a959056f36fe919aac9243340bdad837
Akhilesh1621 <akhileshdevop@gmail.com>
2022-12-27 23:39:21 +0530

    State_Persistence_2

--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 //loggedOut
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(
         child: LoginScreen(),
+      )
+});
+
+//loggedIn
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(
+        child: HomeScreen(),
       )
 });
