3964b4f0bf57a16f14bff5a77a914cefd251ecf4
Akhilesh1621 <akhileshdevop@gmail.com>
2022-12-28 19:22:24 +0530

    Home_Page_UI

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/home/screens/drawers/community_list_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
-      body: Center(child: Text(user.email)),
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: false,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+            ),
+            onPressed: () => displayDrawer(context),
+          );
+        }),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              backgroundImage: NetworkImage(user.profilePic),
+            ),
+          ),
+        ],
+      ),
+      drawer: const CommunityDrawer(),
     );
   }
 }
