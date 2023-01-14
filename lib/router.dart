480ed70b33eadcee118fa0df652efb55d4213a25
Akhilesh1621 <akhilesh@Akhileshs-MacBook-Pro.local>
2023-01-15 00:39:32 +0530

    user_profile

--- lib/router.dart
+++ lib/router.dart
@@ -5,6 +5,7 @@ import 'package:reddit_clone/features/community/screens/create_community_screen.
 import 'package:reddit_clone/features/community/screens/edit_community_screen.dart';
 import 'package:reddit_clone/features/community/screens/mod_tools_screen.dart';
 import 'package:reddit_clone/home/screens/home_screen.dart';
+import 'package:reddit_clone/home/user_profile/screens/user_profile_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 import 'features/community/screens/community_screen.dart';
@@ -57,6 +58,14 @@ final loggedInRoute = RouteMap(
     '/add-mods/:name': (routeData) => MaterialPage(
           child: AddModsScreen(
             name: routeData.pathParameters['name']!,
+          ),
+        ),
+
+//route for user profile screen
+
+    '/u/:uid': (routeData) => MaterialPage(
+          child: UserProfileScreen(
+            uid: routeData.pathParameters['uid']!,
           ),
         ),
   },
